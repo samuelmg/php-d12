c8f7be2b8bc195c1cf338fc0513eb8fc4e15283c
Samuel Mercado <samuel.mg@protonmail.com>
2021-03-17 15:36:14 -0600

    Modifica titulo

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Intro</title>
+    <title>Introducción a PHP</title>
 </head>
 <body>
     <h1>Manejador de Tareas</h1>
